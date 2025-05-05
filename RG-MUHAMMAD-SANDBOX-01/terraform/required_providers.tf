077f753b34d10e7b9f0341ee1c1c985ce22a6149
abhi-harness-cxe <abhishek.sahu@harness.io>
2025-05-05 11:35:51 -0700

    Create required_providers.tf

---
+++ RG-MUHAMMAD-SANDBOX-01/terraform/required_providers.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "s3" {
+    bucket         = "iacm-repro-cxe"   # Replace with your S3 bucket name
+    key            = "workspacerg/terraform.tfstate"      # Path within the bucket (can be folders)
+    region         = "ap-south-1"                      # Replace with your desired AWS region
+    encrypt        = true                             # Encrypt the state file at rest  }
+}
+}
+
+variable "example_var" {
+  description = "An example variable"
+  type        = string
+  default     = "test"
+}
+
+output "test_output" {
+  value = var.example_var
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "your-terraform-state-bucketcxeeeeeeeeeqt"  # Replace with a globally unique bucket name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"                   # Replace with your desired AWS region
+}
+
+
