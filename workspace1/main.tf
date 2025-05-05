2a78c14c34313989450d4c585ef93bb1c1f6a519
abhi-harness-cxe <abhishek.sahu@harness.io>
2025-05-05 09:01:31 -0700

    Update main.tf

--- workspace1/main.tf
+++ workspace1/main.tf
@@ -7,3 +7,26 @@ variable "example_var" {
 output "test_output" {
   value = var.example_var
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "your-terraform-state-bucketcxeeeeeeeee"  # Replace with a globally unique bucket name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
