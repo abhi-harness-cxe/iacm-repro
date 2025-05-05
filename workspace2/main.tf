02075aee7f98546d705ef6995da2db34d0da1864
abhi-harness-cxe <abhishek.sahu@harness.io>
2025-05-05 08:53:17 -0700

    Update main.tf

--- workspace2/main.tf
+++ workspace2/main.tf
@@ -7,3 +7,11 @@ variable "example_var" {
   type        = string
   default     = "test"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "iacm-repro-cxe"   # Replace with your S3 bucket name
+    key            = "workspace2/terraform.tfstate"      # Path within the bucket (can be folders)
+    region         = "ap-south-1"                      # Replace with your desired AWS region
+    encrypt        = true                             # Encrypt the state file at rest  }
+}
