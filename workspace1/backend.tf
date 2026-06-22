bd87c8d8746c9df5455343bfc1ffeda5a0a13c35
abhi-harness-cxe <abhishek.sahu@harness.io>
2026-06-22 15:03:18 +0530

    Remove Terraform backend configuration

--- workspace1/backend.tf
+++ workspace1/backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "iacm-repro-cxe"   # Replace with your S3 bucket name
-    key            = "workspace1/terraform.tfstate"      # Path within the bucket (can be folders)
-    region         = "ap-south-1"                      # Replace with your desired AWS region
-    encrypt        = true                             # Encrypt the state file at rest  }
-}
-}
+
