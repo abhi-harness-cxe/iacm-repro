c3acf9a60c77dcba28fe3798223ef9166ba10a01
abhi-harness-cxe <abhishek.sahu@harness.io>
2026-07-22 16:40:01 +0530

    Update main.tf

--- workspace1/main.tf
+++ workspace1/main.tf
@@ -1,10 +1,82 @@
-variable "example_var" {
-  description = "An example variable"
-  type        = string
-  default     = "test"
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
 }
 
-output "test_output" {
-  value = var.example_var
+########################################
+# Create a local file
+########################################
+
+resource "local_file" "test" {
+  filename = "${path.module}/test.txt"
+  content  = <<EOT
+Hello from Terraform!
+Created at: ${timestamp()}
+EOT
 }
 
+########################################
+# Read the file using a data source
+########################################
+
+data "local_file" "test" {
+  filename = local_file.test.filename
+
+  depends_on = [
+    local_file.test
+  ]
+}
+
+########################################
+# Execute a local command
+########################################
+
+resource "null_resource" "list_directory" {
+
+  depends_on = [
+    local_file.test
+  ]
+
+  provisioner "local-exec" {
+    command = "ls -la > directory_listing.txt"
+  }
+}
+
+########################################
+# Read the generated directory listing
+########################################
+
+data "local_file" "directory_listing" {
+  filename = "${path.module}/directory_listing.txt"
+
+  depends_on = [
+    null_resource.list_directory
+  ]
+}
+
+########################################
+# Outputs
+########################################
+
+output "file_path" {
+  value = local_file.test.filename
+}
+
+output "file_content" {
+  value = data.local_file.test.content
+}
+
+output "directory_listing" {
+  value = data.local_file.directory_listing.content
+}
